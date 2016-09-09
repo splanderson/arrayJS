d1216359f329844cfee45f0125992d5b0169bf2f
Eric Anderson <splanderson@gmail.com>
2016-09-09 15:17:10 -0600

    added arraypractice - devMountainEmployees

--- arraysPractice.js
+++ arraysPractice.js
@@ -311,13 +311,18 @@ var colt = {
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+devMountainEmployees.push(tyler, cahlan, ryan, colt);
+
+console.log(devMountainEmployees);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
-
+for(var i = 0; i < devMountainEmployees.length; i++){
+  if(devMountainEmployees[i] === cahlan){
+    devMountainEmployees.splice(i, 1);
+  }
+}
 
 
 
